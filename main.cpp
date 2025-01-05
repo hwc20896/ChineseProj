9c5dd8ca5e4599c8a798b8c8969c710e65b30d2d
hwc20896 <hwc20896@gmail.com>
2025-01-05 20:58:04 +0800

    Changed the windows title as there are 100 questions now

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char** argv){
     QApplication app(argc, argv);
     Widget window;
     window.show();
-    window.setWindowTitle("中華文化知識40題");
+    window.setWindowTitle("中華文化知識100題");
     return app.exec();
 }
